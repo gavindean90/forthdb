b718eb2d246f63665d69d541e46267a3b14f1570
Gavin Dean <gavindean90@gmail.com>
2026-08-02 22:45:46 -0700

    Export ordinary-file epoch store

--- rust/crates/forthdb-world/src/wrapper.rs
+++ rust/crates/forthdb-world/src/wrapper.rs
@@ -1,5 +1,6 @@
 include!("lib.rs");
 
+mod file_epoch_store;
 mod file_store;
 mod history_lifecycle;
 mod io_uring_store;
@@ -7,6 +8,10 @@ mod mmap_store;
 mod queued;
 mod queued_controller;
 
+pub use file_epoch_store::{
+    EpochFileIo, EpochIoPhase, FileEpochMetrics, FileEpochState, FileEpochStore,
+    FileEpochStoreError, FileEpochSyncPolicy, StdEpochFileIo,
+};
 pub use file_store::{FileCommitStore, FileCommitStoreError};
 pub use io_uring_store::{IoUringCommitStore, IoUringCommitStoreError};
 pub use mmap_store::{MmapCommitStore, MmapCommitStoreError};
