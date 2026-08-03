931d8b722054a3b6509c6b6df7673058e3d40cf1
Gavin Dean <gavindean90@gmail.com>
2026-08-02 19:01:03 -0700

    Expose IoUringCommitStore

--- rust/crates/forthdb-world/src/wrapper.rs
+++ rust/crates/forthdb-world/src/wrapper.rs
@@ -1,7 +1,9 @@
 include!("lib.rs");
 
 mod file_store;
+mod io_uring_store;
 mod mmap_store;
 
 pub use file_store::{FileCommitStore, FileCommitStoreError};
+pub use io_uring_store::{IoUringCommitStore, IoUringCommitStoreError};
 pub use mmap_store::{MmapCommitStore, MmapCommitStoreError};
